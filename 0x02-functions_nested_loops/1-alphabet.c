02ffa47f39e15144738b65699e488a621737fce8
kolynzb <collinsbenda360@gmail.com>
2022-03-15 17:19:52 +0000

    ahh

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*print_alphabet - linajjdjjjd
+*/
+void print_alphabet(void)
+{
+	char alp = 'a';
+
+	while (alp <= 'z')
+	{
+		_putchar (alp);
+		alp++;
+
+	}
+	_putchar ('\n');
+}
